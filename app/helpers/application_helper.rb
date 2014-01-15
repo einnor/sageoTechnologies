d5f0c8942d8bff06262af47523ee98f74015fa24
Ronnie Nyaga <rnafundi@gmail.com>
2014-01-15 23:05:36 +0300

    Added a header file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Return the title on a per page basis
+	def title
+		base_title = "Sageo Technologies"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+
 end
