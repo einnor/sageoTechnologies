bb27b0c9be1f88401b088f30dd4d36b728e09a2e
Ronnie Nyaga <rnafundi@gmail.com>
2014-01-30 08:49:27 +0300

    Vendor compiled assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ SageoTechnologies::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
   
   # Settings for mailer
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
